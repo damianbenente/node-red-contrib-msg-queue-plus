8e1018a4e997c01c537dae175f839bafb778687b
Zentown <zentown@users.noreply.github.com>
2018-02-09 04:53:01 -0300

    fix updateStatus

--- queue-plus.js
+++ queue-plus.js
@@ -13,9 +13,53 @@ module.exports = function (RED) {
 
     node.on('input', function (msg) {
       node.send(process(msg));
+      updateStatus();
+    });
 
-      var myEfficientFn = throttle(function() {
-        if (context.busy){
+    function process(msg) {
+      if (msg.hasOwnProperty("reset")) {
+        context.queue = [];
+        context.busy = false;
+      } else if (msg.hasOwnProperty("trigger")) {
+        if (context.queue.length > 0) {
+          var m = context.queue.shift();
+          var out1 = {};
+          out1.payload = m;
+          if (context.queue.length === 0) {
+            if (node.sendComplete === true) out1.complete = true;
+            context.busy = false;
+          }
+          return [out1, null, {
+            payload: context.queue.length
+          }];
+        } else {
+          context.busy = false;
+          return [null, {
+            payload: 'end'
+          }];
+        }
+      } else {
+        context.queue.push(msg.payload);
+        var x = context.busy;
+        context.busy = true;
+        if (x === false && node.autoTriggerOn === true) {
+          sleep(node.autoTriggerTime).then(() => {
+            node.send(null, {
+              payload: 'start'
+            });
+            msg.trigger = true;
+            node.send(process(msg));
+          })
+        } else {
+          return [null, null, {
+            payload: context.queue.length
+          }];
+        }
+      }
+    }
+
+    var updateStatus = throttle(function () {
+      if (context.busy) {
         node.status({
           fill: "blue",
           shape: "dot",
@@ -28,99 +72,23 @@ module.exports = function (RED) {
           text: context.queue.length
         });
       }
-      }, 250);
-    });
-
-    function process(msg) {
-      if (msg.hasOwnProperty("reset")) {
-        context.queue = [];
-        context.busy = false;
-      }
-      else if (msg.hasOwnProperty("trigger")) {
-        if (context.queue.length > 0) {
-          var m = context.queue.shift();
-          /*node.status({
-            fill: "blue",
-            shape: "dot",
-            text: context.queue.length
-          });*/
-          var out1 = {};
-          out1.payload = m;
-          if (context.queue.length === 0) {
-            if (node.sendComplete === true) out1.complete = true;
-            context.busy = false;
-            /*node.status({
-              fill: "blue",
-              shape: "ring",
-              text: context.queue.length
-            });*/
-          }
-          return [out1, null, {
-            payload: context.queue.length
-          }];
-        } else {
-          context.busy = false;
-          /*node.status({
-            fill: "blue",
-            shape: "ring",
-            text: context.queue.length
-          });*/
-          return [null, {
-            payload: 'end'
-          }];
-        }
-      } else {
-        context.queue.push(msg.payload);
-        /*node.status({
-          fill: "blue",
-          shape: "dot",
-          text: context.queue.length
-        });*/
-        var x = context.busy;
-        context.busy = true;
-        if (x === false && node.autoTriggerOn === true) {
-          sleep(node.autoTriggerTime).then(() => {
-            node.send(null,{ payload: 'start'});
-            msg.trigger = true;
-            node.send(process(msg));
-          })
-        } else {
-          return [null, null, {
-            payload: context.queue.length
-          }];
-        }
-      }
-    }
+    }, 1000);
 
     function sleep(time) {
       return new Promise((resolve) => setTimeout(resolve, time));
     }
 
-    function debounce(func, wait, immediate) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    };
-
     function throttle(func, wait, immediate) {
       var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
+      return function () {
+        var context = this,
+          args = arguments;
+        var later = function () {
           timeout = null;
           if (!immediate) func.apply(context, args);
         };
         var callNow = immediate && !timeout;
-        if ( !timeout ) timeout = setTimeout( later, wait );
+        if (!timeout) timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
       };
     };
